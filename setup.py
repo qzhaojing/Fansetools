982387e743e0947117c7442ecfe8ca4f632ffcb1
qzhaojing <qzhaojing@qq.com>
2025-06-24 01:14:01 +0800

    ready to realease v1.0.0

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='fansetools',
-    version='0.1',
-    package_dir={"": "src"},  # 关键：指定包根目录为src
+    version='v1.0.0',
+    package_dir={"": "src"},  # 指定包根目录为src
     packages=find_packages(where="src"),
     entry_points={
         'console_scripts': [
